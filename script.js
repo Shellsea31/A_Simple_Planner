a1f0ffd6db31459dc6a076c0d72e6c5738865716
Shellsea <elainesbarrera@gmail.com>
2020-10-28 16:47:30 -0700

    loop through array of times, conditionals to change color

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-$("#currentDay").append(moment().format('MMMM Do YYYY'));
+$(document).ready(function() {
 
-let time = (moment().format('h:mm:ss a'));
+    $("#currentDay").append(moment().format('MMMM Do YYYY'));
 
-console.log(time);
+    // this is the current time
+    let now = moment().hour();
 
+    let moments = [moment('9', 'h'), moment('10', 'h'), moment('11', 'h'), moment('12', 'h'), 
+    moment('13', 'h'), moment('14', 'h'), moment('15', 'h'), moment('16', 'h'), moment('17', 'h')];
+
+    // console.log(now) //current time
+    // console.log(moments); //array of possible moments
+
+    // loop through all possible moments
+    for (let i = 0; i < moments.length; i++) {
+        // make them numbers
+        let allMoments = parseInt(moments[i]._i);
+
+        // begin conditionals to change color of input
+        if (allMoments < now) {
+            console.log(allMoments)
+        }
+    }
+    
+
+    
+    
+
+    
+    
+    
+
+    
+
+
+
+
+})
