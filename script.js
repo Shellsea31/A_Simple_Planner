2cee5b2cf3d910971a15fd6fb4b2bd8c00b9fb30
Shellsea <elainesbarrera@gmail.com>
2020-10-28 17:22:21 -0700

    looped through header id instead of moments array

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#currentDay").append(moment().format("MMMM Do YYYY"));
 
-    $("#currentDay").append(moment().format('MMMM Do YYYY'));
+// this is the current time
+  let now = moment().hour();
+// get all headers
+  let headers = document.getElementsByTagName("h4");
 
-    // this is the current time
-    let now = moment().hour();
+  for (let i = 0; i < headers.length; i++) {
+    //   set each header id to allTimes
+    let allTimes = headers[i].id;
+    // make them numbers
+    let parsedTimes = parseInt(allTimes);
 
-    let moments = [moment('9', 'h'), moment('10', 'h'), moment('11', 'h'), moment('12', 'h'), 
-    moment('13', 'h'), moment('14', 'h'), moment('15', 'h'), moment('16', 'h'), moment('17', 'h')];
-
-    // console.log(now) //current time
-    // console.log(moments); //array of possible moments
-
-    // loop through all possible moments
-    for (let i = 0; i < moments.length; i++) {
-        // make them numbers
-        let allMoments = parseInt(moments[i]._i);
-
-        // begin conditionals to change color of input
-        if (allMoments < now) {
-            console.log(allMoments)
-        }
+    // begin conditionals to change color
+    if (parsedTimes === now) {
+      console.log(parsedTimes);
     }
-    
-
-    
-    
-
-    
-    
-    
-
-    
-
-
-
-
-})
+  }
+});
