63a27d010e23f6482efbdb7555061b9a83586df8
Shellsea <elainesbarrera@gmail.com>
2020-10-29 14:15:29 -0700

    object user entry, button loop change text values

--- script.js
+++ script.js
@@ -5,19 +5,67 @@ $(document).ready(function () {
   let now = moment().hour();
 // target all textarea elements by their class
   let textArea = document.querySelectorAll(".col-sm-10");
+  // target all buttons that submit text
+  let submitBtns = document.querySelectorAll(".saveBtn");
+  // console.log(submitBtns)
 
+
+  // loop through all times
   for (let i = 0; i < textArea.length; i++) {
-    // parse each textarea id into number
+    // parse each textarea div id into number
     let parsedTimes = parseInt(textArea[i].id);
 
     // compare parsedTimes to now to change color of textarea
     if (parsedTimes < now) {
       $(textArea[i]).addClass("past");
-    } else if (parsedTimes > now) {
-        console.log(parsedTimes);
+    } 
+    
+    if (parsedTimes > now) {
         $(textArea[i]).addClass("future");
-    } else {
+    } 
+
+    if (parsedTimes === now) {
         $(textArea[i]).addClass("present");
-    }
+    } 
   }
+
+
+  // loop through all buttons
+  for (let i = 0; i < submitBtns.length; i++) {
+
+    // when you click the button
+    $(submitBtns[i]).on("click", function(){
+
+      // set the text area of each submit button
+      let thisTextArea = this.previousElementSibling;
+      console.log(thisTextArea);
+      thisTextArea.append(thisTextArea.value);
+
+      // create an object for all user entries
+      let userNotes = {
+        nine: textArea[0].value,
+        ten: textArea[1].value,
+        eleven: textArea[2].value,
+        twelve: textArea[3].value,
+        one: textArea[4].value,
+        two: textArea[5].value,
+        three: textArea[6].value,
+        four: textArea[7].value,
+        five: textArea[8].value,
+      }
+    
+      console.log(userNotes);
+
+
+      
+      
+
+    })
+    
+     // create object for all entries
+ 
+  }
+
+  
+
 });
